cdmsantos/liferay-portal

LPS-66295 Use the default locale on serialize/deserialize time on front-end

modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder.js
@@ -36,6 +36,10 @@ AUI.add(
 						}
 					},
 
+					defaultLanguageId: {
+						value: themeDisplay.getLanguageId()
+					},
+
 					definition: {
 						validator: Lang.isObject
 					},
@@ -202,6 +206,7 @@ AUI.add(
 								fieldType.get('defaultConfig'),
 								{
 									builder: instance,
+									defaultLanguageId: instance.get('defaultLanguageId'),
 									evaluatorURL: instance.get('evaluatorURL'),
 									getFieldTypeSettingFormContextURL: instance.get('getFieldTypeSettingFormContextURL'),
 									portletNamespace: instance.get('portletNamespace'),

zoltancsaszi/com-liferay-dynamic-data-lists

dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder.js
@@ -33,6 +33,10 @@ AUI.add(
 					dataProviders: {
 					},
 
+					defaultLanguageId: {
+						value: themeDisplay.getDefaultLanguageId()
+					},
+
 					definition: {
 						validator: Lang.isObject
 					},
@@ -157,6 +161,7 @@ AUI.add(
 								{
 									builder: instance,
 									dataProviders: instance.get('dataProviders'),
+									defaultLanguageId: instance.get('defaultLanguageId'),
 									evaluatorURL: instance.get('evaluatorURL'),
 									portletNamespace: instance.get('portletNamespace'),
 									readOnly: true