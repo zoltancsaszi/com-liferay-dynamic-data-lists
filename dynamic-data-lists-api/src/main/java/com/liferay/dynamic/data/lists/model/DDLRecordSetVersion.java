leoadb/liferay-portal

LPS-69156 Regenerate

modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/model/DDLRecordSetVersion.java
@@ -1,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.model;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.annotation.ImplementationClassName;
+import com.liferay.portal.kernel.model.PersistedModel;
+import com.liferay.portal.kernel.util.Accessor;
+
+/**
+ * The extended model interface for the DDLRecordSetVersion service. Represents a row in the &quot;DDLRecordSetVersion&quot; database table, with each column mapped to a property of this class.
+ *
+ * @author Brian Wing Shun Chan
+ * @see DDLRecordSetVersionModel
+ * @see com.liferay.dynamic.data.lists.model.impl.DDLRecordSetVersionImpl
+ * @see com.liferay.dynamic.data.lists.model.impl.DDLRecordSetVersionModelImpl
+ * @generated
+ */
+@ImplementationClassName("com.liferay.dynamic.data.lists.model.impl.DDLRecordSetVersionImpl")
+@ProviderType
+public interface DDLRecordSetVersion extends DDLRecordSetVersionModel,
+	PersistedModel {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this interface directly. Add methods to {@link com.liferay.dynamic.data.lists.model.impl.DDLRecordSetVersionImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+	 */
+	public static final Accessor<DDLRecordSetVersion, Long> RECORD_SET_VERSION_ID_ACCESSOR =
+		new Accessor<DDLRecordSetVersion, Long>() {
+			@Override
+			public Long get(DDLRecordSetVersion ddlRecordSetVersion) {
+				return ddlRecordSetVersion.getRecordSetVersionId();
+			}
+
+			@Override
+			public Class<Long> getAttributeClass() {
+				return Long.class;
+			}
+
+			@Override
+			public Class<DDLRecordSetVersion> getTypeClass() {
+				return DDLRecordSetVersion.class;
+			}
+		};
+
+	public DDLRecordSet getDDLRecordSet()
+		throws com.liferay.portal.kernel.exception.PortalException;
+
+	public com.liferay.dynamic.data.mapping.model.DDMStructureVersion getDDMStructureVersion()
+		throws com.liferay.portal.kernel.exception.PortalException;
+}

modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/service/DDLRecordSetVersionService.java
@@ -1,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.dynamic.data.lists.model.DDLRecordSetVersion;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
+import com.liferay.portal.kernel.security.access.control.AccessControlled;
+import com.liferay.portal.kernel.service.BaseService;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portal.kernel.transaction.Isolation;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
+
+/**
+ * Provides the remote service interface for DDLRecordSetVersion. Methods of this
+ * service are expected to have security checks based on the propagated JAAS
+ * credentials because this service can be accessed remotely.
+ *
+ * @author Brian Wing Shun Chan
+ * @see DDLRecordSetVersionServiceUtil
+ * @see com.liferay.dynamic.data.lists.service.base.DDLRecordSetVersionServiceBaseImpl
+ * @see com.liferay.dynamic.data.lists.service.impl.DDLRecordSetVersionServiceImpl
+ * @generated
+ */
+@AccessControlled
+@JSONWebService
+@OSGiBeanProperties(property =  {
+	"json.web.service.context.name=ddl", "json.web.service.context.path=DDLRecordSetVersion"}, service = DDLRecordSetVersionService.class)
+@ProviderType
+@Transactional(isolation = Isolation.PORTAL, rollbackFor =  {
+	PortalException.class, SystemException.class})
+public interface DDLRecordSetVersionService extends BaseService {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify or reference this interface directly. Always use {@link DDLRecordSetVersionServiceUtil} to access the ddl record set version remote service. Add custom service methods to {@link com.liferay.dynamic.data.lists.service.impl.DDLRecordSetVersionServiceImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+	 */
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public DDLRecordSetVersion getDDLRecordSetVersion(long recordSetVersionId)
+		throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public DDLRecordSetVersion getLatestRecordSetVersion(long recordSetId)
+		throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public int getDDLRecordSetVersionsCount(long recordSetId)
+		throws PortalException;
+
+	/**
+	* Returns the OSGi service identifier.
+	*
+	* @return the OSGi service identifier
+	*/
+	public java.lang.String getOSGiServiceIdentifier();
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<DDLRecordSetVersion> getDDLRecordSetVersions(long recordSetId,
+		int start, int end,
+		OrderByComparator<DDLRecordSetVersion> orderByComparator)
+		throws PortalException;
+}

modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/service/http/DDLRecordSetVersionServiceSoap.java
@@ -1,0 +1,131 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.service.http;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.dynamic.data.lists.service.DDLRecordSetVersionServiceUtil;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.rmi.RemoteException;
+
+/**
+ * Provides the SOAP utility for the
+ * {@link DDLRecordSetVersionServiceUtil} service utility. The
+ * static methods of this class calls the same methods of the service utility.
+ * However, the signatures are different because it is difficult for SOAP to
+ * support certain types.
+ *
+ * <p>
+ * ServiceBuilder follows certain rules in translating the methods. For example,
+ * if the method in the service utility returns a {@link java.util.List}, that
+ * is translated to an array of {@link com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap}.
+ * If the method in the service utility returns a
+ * {@link com.liferay.dynamic.data.lists.model.DDLRecordSetVersion}, that is translated to a
+ * {@link com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap}. Methods that SOAP cannot
+ * safely wire are skipped.
+ * </p>
+ *
+ * <p>
+ * The benefits of using the SOAP utility is that it is cross platform
+ * compatible. SOAP allows different languages like Java, .NET, C++, PHP, and
+ * even Perl, to call the generated services. One drawback of SOAP is that it is
+ * slow because it needs to serialize all calls into a text format (XML).
+ * </p>
+ *
+ * <p>
+ * You can see a list of services at http://localhost:8080/api/axis. Set the
+ * property <b>axis.servlet.hosts.allowed</b> in portal.properties to configure
+ * security.
+ * </p>
+ *
+ * <p>
+ * The SOAP utility is only generated for remote services.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see DDLRecordSetVersionServiceHttp
+ * @see com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap
+ * @see DDLRecordSetVersionServiceUtil
+ * @generated
+ */
+@ProviderType
+public class DDLRecordSetVersionServiceSoap {
+	public static com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap getDDLRecordSetVersion(
+		long recordSetVersionId) throws RemoteException {
+		try {
+			com.liferay.dynamic.data.lists.model.DDLRecordSetVersion returnValue =
+				DDLRecordSetVersionServiceUtil.getDDLRecordSetVersion(recordSetVersionId);
+
+			return com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap.toSoapModel(returnValue);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	public static com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap[] getDDLRecordSetVersions(
+		long recordSetId, int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.lists.model.DDLRecordSetVersion> orderByComparator)
+		throws RemoteException {
+		try {
+			java.util.List<com.liferay.dynamic.data.lists.model.DDLRecordSetVersion> returnValue =
+				DDLRecordSetVersionServiceUtil.getDDLRecordSetVersions(recordSetId,
+					start, end, orderByComparator);
+
+			return com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap.toSoapModels(returnValue);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	public static int getDDLRecordSetVersionsCount(long recordSetId)
+		throws RemoteException {
+		try {
+			int returnValue = DDLRecordSetVersionServiceUtil.getDDLRecordSetVersionsCount(recordSetId);
+
+			return returnValue;
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	public static com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap getLatestRecordSetVersion(
+		long recordSetId) throws RemoteException {
+		try {
+			com.liferay.dynamic.data.lists.model.DDLRecordSetVersion returnValue =
+				DDLRecordSetVersionServiceUtil.getLatestRecordSetVersion(recordSetId);
+
+			return com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap.toSoapModel(returnValue);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(DDLRecordSetVersionServiceSoap.class);
+}

zoltancsaszi/com-liferay-dynamic-data-lists

dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/model/DDLRecordSetVersion.java
@@ -1,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.model;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.annotation.ImplementationClassName;
+import com.liferay.portal.kernel.model.PersistedModel;
+import com.liferay.portal.kernel.util.Accessor;
+
+/**
+ * The extended model interface for the DDLRecordSetVersion service. Represents a row in the &quot;DDLRecordSetVersion&quot; database table, with each column mapped to a property of this class.
+ *
+ * @author Brian Wing Shun Chan
+ * @see DDLRecordSetVersionModel
+ * @see com.liferay.dynamic.data.lists.model.impl.DDLRecordSetVersionImpl
+ * @see com.liferay.dynamic.data.lists.model.impl.DDLRecordSetVersionModelImpl
+ * @generated
+ */
+@ImplementationClassName("com.liferay.dynamic.data.lists.model.impl.DDLRecordSetVersionImpl")
+@ProviderType
+public interface DDLRecordSetVersion extends DDLRecordSetVersionModel,
+	PersistedModel {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this interface directly. Add methods to {@link com.liferay.dynamic.data.lists.model.impl.DDLRecordSetVersionImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+	 */
+	public static final Accessor<DDLRecordSetVersion, Long> RECORD_SET_VERSION_ID_ACCESSOR =
+		new Accessor<DDLRecordSetVersion, Long>() {
+			@Override
+			public Long get(DDLRecordSetVersion ddlRecordSetVersion) {
+				return ddlRecordSetVersion.getRecordSetVersionId();
+			}
+
+			@Override
+			public Class<Long> getAttributeClass() {
+				return Long.class;
+			}
+
+			@Override
+			public Class<DDLRecordSetVersion> getTypeClass() {
+				return DDLRecordSetVersion.class;
+			}
+		};
+
+	public com.liferay.dynamic.data.mapping.model.DDMStructureVersion getDDMStructureVersion()
+		throws com.liferay.portal.kernel.exception.PortalException;
+
+	public DDLRecordSet getRecordSet()
+		throws com.liferay.portal.kernel.exception.PortalException;
+}

dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/service/DDLRecordSetVersionService.java
@@ -1,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.dynamic.data.lists.model.DDLRecordSetVersion;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
+import com.liferay.portal.kernel.security.access.control.AccessControlled;
+import com.liferay.portal.kernel.service.BaseService;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portal.kernel.transaction.Isolation;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
+
+/**
+ * Provides the remote service interface for DDLRecordSetVersion. Methods of this
+ * service are expected to have security checks based on the propagated JAAS
+ * credentials because this service can be accessed remotely.
+ *
+ * @author Brian Wing Shun Chan
+ * @see DDLRecordSetVersionServiceUtil
+ * @see com.liferay.dynamic.data.lists.service.base.DDLRecordSetVersionServiceBaseImpl
+ * @see com.liferay.dynamic.data.lists.service.impl.DDLRecordSetVersionServiceImpl
+ * @generated
+ */
+@AccessControlled
+@JSONWebService
+@OSGiBeanProperties(property =  {
+	"json.web.service.context.name=ddl", "json.web.service.context.path=DDLRecordSetVersion"}, service = DDLRecordSetVersionService.class)
+@ProviderType
+@Transactional(isolation = Isolation.PORTAL, rollbackFor =  {
+	PortalException.class, SystemException.class})
+public interface DDLRecordSetVersionService extends BaseService {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify or reference this interface directly. Always use {@link DDLRecordSetVersionServiceUtil} to access the ddl record set version remote service. Add custom service methods to {@link com.liferay.dynamic.data.lists.service.impl.DDLRecordSetVersionServiceImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+	 */
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public DDLRecordSetVersion getLatestRecordSetVersion(long recordSetId)
+		throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public DDLRecordSetVersion getRecordSetVersion(long recordSetVersionId)
+		throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public int getRecordSetVersionsCount(long recordSetId)
+		throws PortalException;
+
+	/**
+	* Returns the OSGi service identifier.
+	*
+	* @return the OSGi service identifier
+	*/
+	public java.lang.String getOSGiServiceIdentifier();
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<DDLRecordSetVersion> getRecordSetVersions(long recordSetId,
+		int start, int end,
+		OrderByComparator<DDLRecordSetVersion> orderByComparator)
+		throws PortalException;
+}

dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/service/http/DDLRecordSetVersionServiceSoap.java
@@ -1,0 +1,131 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.service.http;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.dynamic.data.lists.service.DDLRecordSetVersionServiceUtil;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.rmi.RemoteException;
+
+/**
+ * Provides the SOAP utility for the
+ * {@link DDLRecordSetVersionServiceUtil} service utility. The
+ * static methods of this class calls the same methods of the service utility.
+ * However, the signatures are different because it is difficult for SOAP to
+ * support certain types.
+ *
+ * <p>
+ * ServiceBuilder follows certain rules in translating the methods. For example,
+ * if the method in the service utility returns a {@link java.util.List}, that
+ * is translated to an array of {@link com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap}.
+ * If the method in the service utility returns a
+ * {@link com.liferay.dynamic.data.lists.model.DDLRecordSetVersion}, that is translated to a
+ * {@link com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap}. Methods that SOAP cannot
+ * safely wire are skipped.
+ * </p>
+ *
+ * <p>
+ * The benefits of using the SOAP utility is that it is cross platform
+ * compatible. SOAP allows different languages like Java, .NET, C++, PHP, and
+ * even Perl, to call the generated services. One drawback of SOAP is that it is
+ * slow because it needs to serialize all calls into a text format (XML).
+ * </p>
+ *
+ * <p>
+ * You can see a list of services at http://localhost:8080/api/axis. Set the
+ * property <b>axis.servlet.hosts.allowed</b> in portal.properties to configure
+ * security.
+ * </p>
+ *
+ * <p>
+ * The SOAP utility is only generated for remote services.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see DDLRecordSetVersionServiceHttp
+ * @see com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap
+ * @see DDLRecordSetVersionServiceUtil
+ * @generated
+ */
+@ProviderType
+public class DDLRecordSetVersionServiceSoap {
+	public static com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap getLatestRecordSetVersion(
+		long recordSetId) throws RemoteException {
+		try {
+			com.liferay.dynamic.data.lists.model.DDLRecordSetVersion returnValue =
+				DDLRecordSetVersionServiceUtil.getLatestRecordSetVersion(recordSetId);
+
+			return com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap.toSoapModel(returnValue);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	public static com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap getRecordSetVersion(
+		long recordSetVersionId) throws RemoteException {
+		try {
+			com.liferay.dynamic.data.lists.model.DDLRecordSetVersion returnValue =
+				DDLRecordSetVersionServiceUtil.getRecordSetVersion(recordSetVersionId);
+
+			return com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap.toSoapModel(returnValue);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	public static com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap[] getRecordSetVersions(
+		long recordSetId, int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.lists.model.DDLRecordSetVersion> orderByComparator)
+		throws RemoteException {
+		try {
+			java.util.List<com.liferay.dynamic.data.lists.model.DDLRecordSetVersion> returnValue =
+				DDLRecordSetVersionServiceUtil.getRecordSetVersions(recordSetId,
+					start, end, orderByComparator);
+
+			return com.liferay.dynamic.data.lists.model.DDLRecordSetVersionSoap.toSoapModels(returnValue);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	public static int getRecordSetVersionsCount(long recordSetId)
+		throws RemoteException {
+		try {
+			int returnValue = DDLRecordSetVersionServiceUtil.getRecordSetVersionsCount(recordSetId);
+
+			return returnValue;
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(DDLRecordSetVersionServiceSoap.class);
+}