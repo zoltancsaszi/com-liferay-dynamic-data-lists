brianchandotcom/liferay-portal

LPS-71192 Adapt views, display contexts and portlet to work with Field sets using Forms functionalities

modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/search_bar.jsp
@@ -17,17 +17,38 @@
 <%@ include file="/admin/init.jsp" %>
 
 <%
-PortletURL portletURL = ddlFormAdminDisplayContext.getPortletURL();
+PortletURL portletURL = renderResponse.createRenderURL();
+
+String currentTab = ParamUtil.getString(request, "currentTab", "forms");
 %>
 
 <aui:nav-bar cssClass="collapse-basic-search" id="toolbar" markupView="lexicon">
 	<aui:nav cssClass="navbar-nav">
-		<aui:nav-item label="forms" selected="<%= true %>" />
+
+		<%
+		portletURL.setParameter("currentTab", "forms");
+		%>
+
+		<aui:nav-item
+			href="<%= portletURL.toString() %>"
+			label="forms"
+			selected='<%= currentTab.equals("forms") %>'
+		/>
+
+		<%
+		portletURL.setParameter("currentTab", "field-sets");
+		%>
+
+		<aui:nav-item
+			href="<%= portletURL.toString() %>"
+			label="field-sets"
+			selected='<%= currentTab.equals("field-sets") %>'
+		/>
 	</aui:nav>
 
 	<c:if test="<%= ddlFormAdminDisplayContext.isShowSearch() %>">
 		<aui:nav-bar-search>
-			<aui:form action="<%= portletURL.toString() %>" method="post" name="fm1">
+			<aui:form action="<%= ddlFormAdminDisplayContext.getPortletURL() %>" method="post" name="fm1">
 				<liferay-ui:input-search markupView="lexicon" />
 			</aui:form>
 		</aui:nav-bar-search>

modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/toolbar.jsp
@@ -18,11 +18,13 @@
 
 <%
 PortletURL portletURL = ddlFormAdminDisplayContext.getPortletURL();
+
+String currentTab = ParamUtil.getString(request, "currentTab", "forms");
 %>
 
 <liferay-frontend:management-bar
 	includeCheckBox="<%= true %>"
-	searchContainerId="ddlRecordSet"
+	searchContainerId="<%= ddlFormAdminDisplayContext.getSearchContainerId() %>"
 >
 	<liferay-frontend:management-bar-buttons>
 		<liferay-util:include page="/admin/display_style_buttons.jsp" servletContext="<%= application %>" />
@@ -43,21 +45,48 @@ PortletURL portletURL = ddlFormAdminDisplayContext.getPortletURL();
 	</liferay-frontend:management-bar-filters>
 
 	<liferay-frontend:management-bar-action-buttons>
-		<liferay-frontend:management-bar-button href='<%= "javascript:" + renderResponse.getNamespace() + "deleteRecordSets();" %>' icon="trash" label="delete" />
+		<c:choose>
+			<c:when test='<%= currentTab.equals("forms") %>'>
+				<liferay-frontend:management-bar-button href='<%= "javascript:" + renderResponse.getNamespace() + "deleteRecordSets();" %>' icon="trash" label="delete" />
+			</c:when>
+			<c:when test='<%= currentTab.equals("field-sets") %>'>
+				<liferay-frontend:management-bar-button href='<%= "javascript:" + renderResponse.getNamespace() + "deleteStructures();" %>' icon="trash" label="delete" />
+			</c:when>
+		</c:choose>
 	</liferay-frontend:management-bar-action-buttons>
 </liferay-frontend:management-bar>
 
-<aui:script>
-	function <portlet:namespace />deleteRecordSets() {
-		if (confirm('<%= UnicodeLanguageUtil.get(request, "are-you-sure-you-want-to-delete-this") %>')) {
-			var form = AUI.$(document.<portlet:namespace />searchContainerForm);
+<c:choose>
+	<c:when test='<%= currentTab.equals("forms") %>'>
+		<aui:script>
+			function <portlet:namespace />deleteRecordSets() {
+				if (confirm('<%= UnicodeLanguageUtil.get(request, "are-you-sure-you-want-to-delete-this") %>')) {
+					var form = AUI.$(document.<portlet:namespace />searchContainerForm);
 
-			var searchContainer = AUI.$('#<portlet:namespace />ddlRecordSet', form);
+					var searchContainer = AUI.$('#<portlet:namespace /><%= ddlFormAdminDisplayContext.getSearchContainerId() %>', form);
 
-			form.attr('method', 'post');
-			form.fm('deleteRecordSetIds').val(Liferay.Util.listCheckedExcept(searchContainer, '<portlet:namespace />allRowIds'));
+					form.attr('method', 'post');
+					form.fm('deleteRecordSetIds').val(Liferay.Util.listCheckedExcept(searchContainer, '<portlet:namespace />allRowIds'));
 
-			submitForm(form, '<portlet:actionURL name="deleteRecordSet"><portlet:param name="mvcPath" value="/admin/view.jsp" /><portlet:param name="redirect" value="<%= currentURL %>" /></portlet:actionURL>');
-		}
-	}
-</aui:script>
+					submitForm(form, '<portlet:actionURL name="deleteRecordSet"><portlet:param name="mvcPath" value="/admin/view.jsp" /><portlet:param name="redirect" value="<%= currentURL %>" /></portlet:actionURL>');
+				}
+			}
+		</aui:script>
+	</c:when>
+	<c:when test='<%= currentTab.equals("field-sets") %>'>
+		<aui:script>
+			function <portlet:namespace />deleteStructures() {
+				if (confirm('<%= UnicodeLanguageUtil.get(request, "are-you-sure-you-want-to-delete-this") %>')) {
+					var form = AUI.$(document.<portlet:namespace />searchContainerForm);
+
+					var searchContainer = AUI.$('#<portlet:namespace /><%= ddlFormAdminDisplayContext.getSearchContainerId() %>', form);
+
+					form.attr('method', 'post');
+					form.fm('deleteStructureIds').val(Liferay.Util.listCheckedExcept(searchContainer, '<portlet:namespace />allRowIds'));
+
+					submitForm(form, '<portlet:actionURL name="deleteStructure"><portlet:param name="mvcPath" value="/admin/view.jsp" /><portlet:param name="currentTab" value="field-sets" /><portlet:param name="redirect" value="<%= currentURL %>" /></portlet:actionURL>');
+				}
+			}
+		</aui:script>
+	</c:when>
+</c:choose>

modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/view.jsp
@@ -17,145 +17,18 @@
 <%@ include file="/admin/init.jsp" %>
 
 <%
-String displayStyle = ddlFormAdminDisplayContext.getDisplayStyle();
-
-PortletURL portletURL = ddlFormAdminDisplayContext.getPortletURL();
-
-portletURL.setParameter("displayStyle", displayStyle);
+String currentTab = ParamUtil.getString(request, "currentTab", "forms");
 %>
 
 <liferay-util:include page="/admin/search_bar.jsp" servletContext="<%= application %>" />
 
 <liferay-util:include page="/admin/toolbar.jsp" servletContext="<%= application %>" />
 
-<div class="container-fluid-1280" id="<portlet:namespace />formContainer">
-	<aui:form action="<%= portletURL.toString() %>" method="post" name="searchContainerForm">
-		<aui:input name="redirect" type="hidden" value="<%= portletURL.toString() %>" />
-		<aui:input name="deleteRecordSetIds" type="hidden" />
-
-		<liferay-ui:search-container
-			id="ddlRecordSet"
-			rowChecker="<%= new EmptyOnClickRowChecker(renderResponse) %>"
-			searchContainer="<%= ddlFormAdminDisplayContext.getRecordSetSearch() %>"
-		>
-			<liferay-ui:search-container-row
-				className="com.liferay.dynamic.data.lists.model.DDLRecordSet"
-				cssClass="entry-display-style"
-				keyProperty="recordSetId"
-				modelVar="recordSet"
-			>
-				<portlet:renderURL var="rowURL">
-					<portlet:param name="mvcPath" value="/admin/edit_record_set.jsp" />
-					<portlet:param name="redirect" value="<%= currentURL %>" />
-					<portlet:param name="recordSetId" value="<%= String.valueOf(recordSet.getRecordSetId()) %>" />
-					<portlet:param name="displayStyle" value="<%= displayStyle %>" />
-				</portlet:renderURL>
-
-				<c:choose>
-					<c:when test='<%= displayStyle.equals("descriptive") %>'>
-						<liferay-ui:search-container-column-icon
-							cssClass="asset-icon"
-							icon="forms"
-						/>
-
-						<liferay-ui:search-container-column-jsp
-							colspan="<%= 2 %>"
-							href="<%= rowURL %>"
-							path="/admin/view_record_set_descriptive.jsp"
-						/>
-
-						<liferay-ui:search-container-column-jsp
-							path="/admin/record_set_action.jsp"
-						/>
-					</c:when>
-					<c:otherwise>
-						<liferay-ui:search-container-column-text
-							cssClass="table-cell-content"
-							href="<%= rowURL %>"
-							name="name"
-							value="<%= HtmlUtil.escape(recordSet.getName(locale)) %>"
-						/>
-
-						<liferay-ui:search-container-column-text
-							cssClass="table-cell-content"
-							name="description"
-							value="<%= HtmlUtil.escape(recordSet.getDescription(locale)) %>"
-						/>
-
-						<liferay-ui:search-container-column-date
-							name="modified-date"
-							value="<%= recordSet.getModifiedDate() %>"
-						/>
-
-						<liferay-ui:search-container-column-jsp
-							path="/admin/record_set_action.jsp"
-						/>
-					</c:otherwise>
-				</c:choose>
-			</liferay-ui:search-container-row>
-
-			<liferay-ui:search-iterator displayStyle="<%= displayStyle %>" markupView="lexicon" />
-		</liferay-ui:search-container>
-	</aui:form>
-</div>
-
-<c:if test="<%= ddlFormAdminDisplayContext.isShowAddRecordSetButton() %>">
-	<portlet:renderURL var="addRecordSetURL">
-		<portlet:param name="mvcPath" value="/admin/edit_record_set.jsp" />
-		<portlet:param name="redirect" value="<%= currentURL %>" />
-		<portlet:param name="groupId" value="<%= String.valueOf(scopeGroupId) %>" />
-	</portlet:renderURL>
-
-	<liferay-frontend:add-menu>
-		<liferay-frontend:add-menu-item title='<%= LanguageUtil.get(request, "new-form") %>' url="<%= addRecordSetURL.toString() %>" />
-	</liferay-frontend:add-menu>
-</c:if>
-
-<%@ include file="/admin/export_record_set.jspf" %>
-
-<%@ include file="/admin/copy_form_publish_url.jspf" %>
-
-<aui:script use="liferay-ddl-form-builder-copy-publish-form-url-popover">
-	var A = AUI();
-
-	var copyPublishFormURLPopover = new Liferay.DDL.FormBuilderCopyPublishFormURLPopover(
-		{
-			portletNamespace: '<portlet:namespace />'
-		}
-	);
-
-	Liferay.on(
-		'<portlet:namespace />copyFormURL',
-		function(event) {
-
-			if (copyPublishFormURLPopover.isVisible()) {
-				copyPublishFormURLPopover.hide();
-			}
-
-			copyPublishFormURLPopover.set('publishURL', event.url);
-
-			copyPublishFormURLPopover.setAlign(
-				{
-					node: Liferay.Menu._INSTANCE._activeTrigger,
-					points: [A.WidgetPositionAlign.TR, A.WidgetPositionAlign.TR]
-				}
-			);
-
-			copyPublishFormURLPopover.show();
-		}
-	);
-
-	A.all('.searchcontainer-content .dropdown-toggle.icon-monospaced').before(
-		'click',
-		function() {
-			copyPublishFormURLPopover.hide();
-		}
-	);
-
-	Liferay.on(
-		'destroyPortlet',
-		function() {
-			copyPublishFormURLPopover.destroy();
-		}
-	);
-</aui:script>
+<c:choose>
+	<c:when test='<%= currentTab.equals("forms") %>'>
+		<liferay-util:include page="/admin/view_forms.jsp" servletContext="<%= application %>" />
+	</c:when>
+	<c:when test='<%= currentTab.equals("field-sets") %>'>
+		<liferay-util:include page="/admin/view_field_sets.jsp" servletContext="<%= application %>" />
+	</c:when>
+</c:choose>

zoltancsaszi/com-liferay-dynamic-data-lists

dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/search_bar.jsp
@@ -17,17 +17,38 @@
 <%@ include file="/admin/init.jsp" %>
 
 <%
-PortletURL portletURL = ddlFormAdminDisplayContext.getPortletURL();
+PortletURL portletURL = renderResponse.createRenderURL();
+
+String currentTab = ParamUtil.getString(request, "currentTab", "forms");
 %>
 
 <aui:nav-bar cssClass="collapse-basic-search" id="toolbar" markupView="lexicon">
 	<aui:nav cssClass="navbar-nav">
-		<aui:nav-item label="forms" selected="<%= true %>" />
+
+		<%
+		portletURL.setParameter("currentTab", "forms");
+		%>
+
+		<aui:nav-item
+			href="<%= portletURL.toString() %>"
+			label="forms"
+			selected='<%= currentTab.equals("forms") %>'
+		/>
+
+		<%
+		portletURL.setParameter("currentTab", "field-set");
+		%>
+
+		<aui:nav-item
+			href="<%= portletURL.toString() %>"
+			label="field-sets"
+			selected='<%= currentTab.equals("field-set") %>'
+		/>
 	</aui:nav>
 
 	<c:if test="<%= ddlFormAdminDisplayContext.isShowSearch() %>">
 		<aui:nav-bar-search>
-			<aui:form action="<%= portletURL.toString() %>" method="post" name="fm1">
+			<aui:form action="<%= ddlFormAdminDisplayContext.getPortletURL() %>" method="post" name="fm1">
 				<liferay-ui:input-search markupView="lexicon" />
 			</aui:form>
 		</aui:nav-bar-search>

dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/toolbar.jsp
@@ -18,11 +18,13 @@
 
 <%
 PortletURL portletURL = ddlFormAdminDisplayContext.getPortletURL();
+
+String currentTab = ParamUtil.getString(request, "currentTab", "forms");
 %>
 
 <liferay-frontend:management-bar
 	includeCheckBox="<%= true %>"
-	searchContainerId="ddlRecordSet"
+	searchContainerId="<%= ddlFormAdminDisplayContext.getSearchContainerId() %>"
 >
 	<liferay-frontend:management-bar-buttons>
 		<liferay-util:include page="/admin/display_style_buttons.jsp" servletContext="<%= application %>" />
@@ -43,21 +45,48 @@ PortletURL portletURL = ddlFormAdminDisplayContext.getPortletURL();
 	</liferay-frontend:management-bar-filters>
 
 	<liferay-frontend:management-bar-action-buttons>
-		<liferay-frontend:management-bar-button href='<%= "javascript:" + renderResponse.getNamespace() + "deleteRecordSets();" %>' icon="trash" label="delete" />
+		<c:choose>
+			<c:when test='<%= currentTab.equals("forms") %>'>
+				<liferay-frontend:management-bar-button href='<%= "javascript:" + renderResponse.getNamespace() + "deleteRecordSets();" %>' icon="trash" label="delete" />
+			</c:when>
+			<c:when test='<%= currentTab.equals("field-set") %>'>
+				<liferay-frontend:management-bar-button href='<%= "javascript:" + renderResponse.getNamespace() + "deleteStructures();" %>' icon="trash" label="delete" />
+			</c:when>
+		</c:choose>
 	</liferay-frontend:management-bar-action-buttons>
 </liferay-frontend:management-bar>
 
-<aui:script>
-	function <portlet:namespace />deleteRecordSets() {
-		if (confirm('<%= UnicodeLanguageUtil.get(request, "are-you-sure-you-want-to-delete-this") %>')) {
-			var form = AUI.$(document.<portlet:namespace />searchContainerForm);
+<c:choose>
+	<c:when test='<%= currentTab.equals("forms") %>'>
+		<aui:script>
+			function <portlet:namespace />deleteRecordSets() {
+				if (confirm('<%= UnicodeLanguageUtil.get(request, "are-you-sure-you-want-to-delete-this") %>')) {
+					var form = AUI.$(document.<portlet:namespace />searchContainerForm);
 
-			var searchContainer = AUI.$('#<portlet:namespace />ddlRecordSet', form);
+					var searchContainer = AUI.$('#<portlet:namespace /><%= ddlFormAdminDisplayContext.getSearchContainerId() %>', form);
 
-			form.attr('method', 'post');
-			form.fm('deleteRecordSetIds').val(Liferay.Util.listCheckedExcept(searchContainer, '<portlet:namespace />allRowIds'));
+					form.attr('method', 'post');
+					form.fm('deleteRecordSetIds').val(Liferay.Util.listCheckedExcept(searchContainer, '<portlet:namespace />allRowIds'));
 
-			submitForm(form, '<portlet:actionURL name="deleteRecordSet"><portlet:param name="mvcPath" value="/admin/view.jsp" /><portlet:param name="redirect" value="<%= currentURL %>" /></portlet:actionURL>');
-		}
-	}
-</aui:script>
+					submitForm(form, '<portlet:actionURL name="deleteRecordSet"><portlet:param name="mvcPath" value="/admin/view.jsp" /><portlet:param name="redirect" value="<%= currentURL %>" /></portlet:actionURL>');
+				}
+			}
+		</aui:script>
+	</c:when>
+	<c:when test='<%= currentTab.equals("field-set") %>'>
+		<aui:script>
+			function <portlet:namespace />deleteStructures() {
+				if (confirm('<%= UnicodeLanguageUtil.get(request, "are-you-sure-you-want-to-delete-this") %>')) {
+					var form = AUI.$(document.<portlet:namespace />searchContainerForm);
+
+					var searchContainer = AUI.$('#<portlet:namespace /><%= ddlFormAdminDisplayContext.getSearchContainerId() %>', form);
+
+					form.attr('method', 'post');
+					form.fm('deleteStructureIds').val(Liferay.Util.listCheckedExcept(searchContainer, '<portlet:namespace />allRowIds'));
+
+					submitForm(form, '<portlet:actionURL name="deleteStructure"><portlet:param name="mvcPath" value="/admin/view.jsp" /><portlet:param name="currentTab" value="field-set" /><portlet:param name="redirect" value="<%= currentURL %>" /></portlet:actionURL>');
+				}
+			}
+		</aui:script>
+	</c:when>
+</c:choose>

dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/view.jsp
@@ -17,145 +17,18 @@
 <%@ include file="/admin/init.jsp" %>
 
 <%
-String displayStyle = ddlFormAdminDisplayContext.getDisplayStyle();
-
-PortletURL portletURL = ddlFormAdminDisplayContext.getPortletURL();
-
-portletURL.setParameter("displayStyle", displayStyle);
+String currentTab = ParamUtil.getString(request, "currentTab", "forms");
 %>
 
 <liferay-util:include page="/admin/search_bar.jsp" servletContext="<%= application %>" />
 
 <liferay-util:include page="/admin/toolbar.jsp" servletContext="<%= application %>" />
 
-<div class="container-fluid-1280" id="<portlet:namespace />formContainer">
-	<aui:form action="<%= portletURL.toString() %>" method="post" name="searchContainerForm">
-		<aui:input name="redirect" type="hidden" value="<%= portletURL.toString() %>" />
-		<aui:input name="deleteRecordSetIds" type="hidden" />
-
-		<liferay-ui:search-container
-			id="ddlRecordSet"
-			rowChecker="<%= new EmptyOnClickRowChecker(renderResponse) %>"
-			searchContainer="<%= ddlFormAdminDisplayContext.getRecordSetSearch() %>"
-		>
-			<liferay-ui:search-container-row
-				className="com.liferay.dynamic.data.lists.model.DDLRecordSet"
-				cssClass="entry-display-style"
-				keyProperty="recordSetId"
-				modelVar="recordSet"
-			>
-				<portlet:renderURL var="rowURL">
-					<portlet:param name="mvcPath" value="/admin/edit_record_set.jsp" />
-					<portlet:param name="redirect" value="<%= currentURL %>" />
-					<portlet:param name="recordSetId" value="<%= String.valueOf(recordSet.getRecordSetId()) %>" />
-					<portlet:param name="displayStyle" value="<%= displayStyle %>" />
-				</portlet:renderURL>
-
-				<c:choose>
-					<c:when test='<%= displayStyle.equals("descriptive") %>'>
-						<liferay-ui:search-container-column-icon
-							cssClass="asset-icon"
-							icon="forms"
-						/>
-
-						<liferay-ui:search-container-column-jsp
-							colspan="<%= 2 %>"
-							href="<%= rowURL %>"
-							path="/admin/view_record_set_descriptive.jsp"
-						/>
-
-						<liferay-ui:search-container-column-jsp
-							path="/admin/record_set_action.jsp"
-						/>
-					</c:when>
-					<c:otherwise>
-						<liferay-ui:search-container-column-text
-							cssClass="table-cell-content"
-							href="<%= rowURL %>"
-							name="name"
-							value="<%= HtmlUtil.escape(recordSet.getName(locale)) %>"
-						/>
-
-						<liferay-ui:search-container-column-text
-							cssClass="table-cell-content"
-							name="description"
-							value="<%= HtmlUtil.escape(recordSet.getDescription(locale)) %>"
-						/>
-
-						<liferay-ui:search-container-column-date
-							name="modified-date"
-							value="<%= recordSet.getModifiedDate() %>"
-						/>
-
-						<liferay-ui:search-container-column-jsp
-							path="/admin/record_set_action.jsp"
-						/>
-					</c:otherwise>
-				</c:choose>
-			</liferay-ui:search-container-row>
-
-			<liferay-ui:search-iterator displayStyle="<%= displayStyle %>" markupView="lexicon" />
-		</liferay-ui:search-container>
-	</aui:form>
-</div>
-
-<c:if test="<%= ddlFormAdminDisplayContext.isShowAddRecordSetButton() %>">
-	<portlet:renderURL var="addRecordSetURL">
-		<portlet:param name="mvcPath" value="/admin/edit_record_set.jsp" />
-		<portlet:param name="redirect" value="<%= currentURL %>" />
-		<portlet:param name="groupId" value="<%= String.valueOf(scopeGroupId) %>" />
-	</portlet:renderURL>
-
-	<liferay-frontend:add-menu>
-		<liferay-frontend:add-menu-item title='<%= LanguageUtil.get(request, "new-form") %>' url="<%= addRecordSetURL.toString() %>" />
-	</liferay-frontend:add-menu>
-</c:if>
-
-<%@ include file="/admin/export_record_set.jspf" %>
-
-<%@ include file="/admin/copy_form_publish_url.jspf" %>
-
-<aui:script use="liferay-ddl-form-builder-copy-publish-form-url-popover">
-	var A = AUI();
-
-	var copyPublishFormURLPopover = new Liferay.DDL.FormBuilderCopyPublishFormURLPopover(
-		{
-			portletNamespace: '<portlet:namespace />'
-		}
-	);
-
-	Liferay.on(
-		'<portlet:namespace />copyFormURL',
-		function(event) {
-
-			if (copyPublishFormURLPopover.isVisible()) {
-				copyPublishFormURLPopover.hide();
-			}
-
-			copyPublishFormURLPopover.set('publishURL', event.url);
-
-			copyPublishFormURLPopover.setAlign(
-				{
-					node: Liferay.Menu._INSTANCE._activeTrigger,
-					points: [A.WidgetPositionAlign.TR, A.WidgetPositionAlign.TR]
-				}
-			);
-
-			copyPublishFormURLPopover.show();
-		}
-	);
-
-	A.all('.searchcontainer-content .dropdown-toggle.icon-monospaced').before(
-		'click',
-		function() {
-			copyPublishFormURLPopover.hide();
-		}
-	);
-
-	Liferay.on(
-		'destroyPortlet',
-		function() {
-			copyPublishFormURLPopover.destroy();
-		}
-	);
-</aui:script>
+<c:choose>
+	<c:when test='<%= currentTab.equals("forms") %>'>
+		<liferay-util:include page="/admin/view_form.jsp" servletContext="<%= application %>" />
+	</c:when>
+	<c:when test='<%= currentTab.equals("field-set") %>'>
+		<liferay-util:include page="/admin/view_field_set.jsp" servletContext="<%= application %>" />
+	</c:when>
+</c:choose>