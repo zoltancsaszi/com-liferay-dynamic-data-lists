b4f4dbeac97e3bc901eebd5650edcf041c8f6c8c
Gergely Mathe <gergely.mathe@liferay.com>
2017-11-21 15:23:20 +0100

    LPS-76009 Add test for getDataPortletPreferences method

---
+++ dynamic-data-lists-test/src/testIntegration/java/com/liferay/dynamic/data/lists/web/internal/exportimport/data/handler/test/DDLDisplayPortletDataHandlerTest.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.web.internal.exportimport.data.handler.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.lists.constants.DDLPortletKeys;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.lar.test.BasePortletDataHandlerTestCase;
+import com.liferay.portal.service.test.ServiceTestUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Gergely Mathe
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class DDLDisplayPortletDataHandlerTest
+	extends BasePortletDataHandlerTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		ServiceTestUtil.setUser(TestPropsValues.getUser());
+
+		super.setUp();
+	}
+
+	@Override
+	protected void addStagedModels() throws Exception {
+		return;
+	}
+
+	@Override
+	protected String[] getDataPortletPreferences() {
+		return new String[] {
+			"displayDDMTemplateId", "formDDMTemplateId", "recordSetId"};
+	}
+
+	@Override
+	protected String getPortletId() {
+		return DDLPortletKeys.DYNAMIC_DATA_LISTS_DISPLAY;
+	}
+
+}
